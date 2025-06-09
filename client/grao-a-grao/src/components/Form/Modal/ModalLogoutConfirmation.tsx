894b7dd36d935db3f87375538a6e83e217ab57a6
Richard Torres(JunkLab) <richard.ms.torres@gmail.com>
2025-06-09 13:56:28 -0300

    conflict in using Radix UI

--- client/grao-a-grao/src/components/Form/Modal/ModalLogoutConfirmation.tsx
+++ client/grao-a-grao/src/components/Form/Modal/ModalLogoutConfirmation.tsx
@@ -3,6 +3,7 @@ import { LogOut } from "lucide-react";
 import { logout } from "@/util/util";
 import { useRouter } from "next/navigation";
 import { SidebarMenuButton } from "@/components/ui/sidebar";
+import { Button } from "@radix-ui/themes";
 
 export function ModalFormLogout() {
   const router = useRouter();
@@ -21,38 +22,41 @@ export function ModalFormLogout() {
 
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="fixed inset-0 backdrop-blur-sm z-40" />
-        <AlertDialog.Content className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-modal text-card p-6 rounded-xl shadow-xl w-[90vw] max-w-sm">
-          <AlertDialog.Title className="text-lg font-semibold text-muted-foreground ">
-            Deseja finalizar a sessão?
-          </AlertDialog.Title>
+<AlertDialog.Content
+  className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-modal text-card p-6 rounded-xl shadow-xl w-[90vw] max-w-sm">
+    <AlertDialog.Title className="text-lg font-semibold text-muted-foreground ">
+      Deseja finalizar a sessão?
+    </AlertDialog.Title>
 
-          <AlertDialog.Description className="text-sm text-muted-foreground mt-2">
-            Você será desconectado e redirecionado para a tela de login.
-          </AlertDialog.Description>
+    <AlertDialog.Description className="text-sm text-muted-foreground mt-2">
+      Você será desconectado e redirecionado para a tela de login.
+    </AlertDialog.Description>
 
-          <div className="mt-6 flex justify-end gap-3">
-            <AlertDialog.Cancel asChild>
-              <button
-                className="px-4 py-2 rounded-md border border-border bg-muted text-foreground hover:bg-muted/80 transition cursor-pointer"
-              >
-                Cancelar
-              </button>
-            </AlertDialog.Cancel>
-            
-            <AlertDialog.Action asChild>
-              <button
-                className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-700 transition cursor-pointer"
-                onClick={() => {
-                  logout();
-                  router.push("/login");
-                }}
-              >
-                Sair
-              </button>
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
+    <div className="mt-6 flex justify-end gap-3">
+      <AlertDialog.Cancel asChild>
+        <Button
+          color="gray"
+          radius="medium"
+          style={{ cursor: "pointer" }}>
+            Cancelar
+        </Button>
+      </AlertDialog.Cancel>
+
+      <AlertDialog.Action asChild>
+        <button className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-700 transition cursor-pointer"
+          onClick={() => {
+            logout();
+            router.push("/login");
+          }}
+        >
+          Sair
+        </button>
+      </AlertDialog.Action>
+    </div>
+</AlertDialog.Content>
+
       </AlertDialog.Portal>
     </AlertDialog.Root>
   );
 }
+ 
