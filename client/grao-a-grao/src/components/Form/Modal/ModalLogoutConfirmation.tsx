39af96ab160fadb4968f2c028eaa0410faba7c14
Richard Torres(JunkLab) <richard.ms.torres@gmail.com>
2025-06-10 16:10:09 -0300

    RadixUI usage fixes

--- client/grao-a-grao/src/components/Form/Modal/ModalLogoutConfirmation.tsx
+++ client/grao-a-grao/src/components/Form/Modal/ModalLogoutConfirmation.tsx
@@ -1,9 +1,8 @@
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { LogOut } from "lucide-react";
 import { logout } from "@/util/util";
 import { useRouter } from "next/navigation";
 import { SidebarMenuButton } from "@/components/ui/sidebar";
-import { Button, Flex } from "@radix-ui/themes";
+import { Button, Flex, AlertDialog } from "@radix-ui/themes";
 
 export function ModalFormLogout() {
   const router = useRouter();
@@ -11,7 +10,7 @@ export function ModalFormLogout() {
 
   return (
     <AlertDialog.Root>
-      <AlertDialog.Trigger asChild>
+      <AlertDialog.Trigger>
         <SidebarMenuButton className="cursor-pointer" asChild>
           <div>
             <LogOut />
@@ -20,43 +19,33 @@ export function ModalFormLogout() {
         </SidebarMenuButton>
       </AlertDialog.Trigger>
 
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="fixed inset-0 backdrop-blur-sm z-40" />
-<AlertDialog.Content
-  className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-modal text-card p-6 rounded-xl shadow-xl w-[90vw] max-w-sm">
-    <AlertDialog.Title className="text-lg font-semibold text-muted-foreground ">
-      Deseja finalizar a sessão?
-    </AlertDialog.Title>
+      <AlertDialog.Content style={{ zIndex:50 }}>
+        <AlertDialog.Title>Deseja finalizar a sessão?</AlertDialog.Title>
 
-    <AlertDialog.Description className="text-sm text-muted-foreground mt-2">
-      Você será desconectado e redirecionado para a tela de login.
-    </AlertDialog.Description>
+        <AlertDialog.Description>
+          Você será desconectado e redirecionado para a tela de login.
+        </AlertDialog.Description>
 
-    <Flex justify="end" gap="3"> 
-      <AlertDialog.Cancel asChild>
-        <Button
-          color="gray"
-          radius="medium"
-          style={{ cursor: "pointer" }}>
-            Cancelar
-        </Button>
-      </AlertDialog.Cancel>
+        <Flex justify="end" gap="3" mt="4">
+          <AlertDialog.Cancel>
+            <Button color="gray" style={{cursor: "pointer"}}>
+              Cancelar
+            </Button>
+          </AlertDialog.Cancel>
 
-      <AlertDialog.Action asChild>
-        <Button color="red"
-          onClick={() => {
-            logout();
-            router.push("/login");
-          }}
-        >
-          Sair
-        </Button>
-      </AlertDialog.Action>
-    </Flex>
-</AlertDialog.Content>
-
-      </AlertDialog.Portal>
+          <AlertDialog.Action>
+            <Button
+              color="red" style={{cursor: "pointer"}}
+              onClick={() => {
+                logout();
+                router.push("/login");
+              }}
+            >
+              Sair
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
     </AlertDialog.Root>
   );
 }
- 
