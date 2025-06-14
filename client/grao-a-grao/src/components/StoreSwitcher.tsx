a145c2ce70cf19542c4cd257c934b051b8bc6f8e
Richard Torres(JunkLab) <richard.ms.torres@gmail.com>
2025-06-14 16:00:26 -0300

    animation of sidebar elements & Radix.cursor.css

--- client/grao-a-grao/src/components/StoreSwitcher.tsx
+++ client/grao-a-grao/src/components/StoreSwitcher.tsx
@@ -1,73 +1,81 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { useState } from "react"
-import { Check, ChevronDown, GalleryVerticalEnd } from "lucide-react"
+import * as React from "react";
+import { useState } from "react";
+import { Check, ChevronDown, GalleryVerticalEnd } from "lucide-react";
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { StoreModel } from "@/types/store"
-import { useStoreContext } from "@/context/StoreContext"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { StoreModel } from "@/types/store";
+import { useStoreContext } from "@/context/StoreContext";
 
 export function StoreSwitcher({
-    onStoreChange,
+  onStoreChange,
 }: {
-    stores: StoreModel[]
-    defaultStore: StoreModel
-    onStoreChange: (store: StoreModel) => void
+  stores: StoreModel[];
+  defaultStore: StoreModel;
+  onStoreChange: (store: StoreModel) => void;
 }) {
-    const { stores, selectedStore, setSelectedStore } = useStoreContext();
-    const [menuOpen, setMenuOpen] = useState(false);
+  const { stores, selectedStore, setSelectedStore } = useStoreContext();
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <SidebarMenu>
-            <SidebarMenuItem>
-                <DropdownMenu onOpenChange={setMenuOpen}>
-                    <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton
-                            size="lg"
-                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
-                        >
-                            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-accent">
-                                <GalleryVerticalEnd className="size-4" />
-                            </div>
-                            <div className="flex flex-col gap-0.5 leading-none">
-                                <span className="font-semibold">Loja</span>
-                                <span>{selectedStore ? selectedStore.name : "Selecione uma loja"}</span>
-                            </div>
-                            <ChevronDown
-                                className={`ml-auto transition-transform duration-500 ${menuOpen ? "rotate-180" : ""}`}
-                            />
-                        </SidebarMenuButton>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                        className="w-[var(--radix-dropdown-menu-trigger-width)] bg-[var(--muted)] text-[var(--foreground)]
-"
-                        align="start"
-                    >
-                        {stores.map((st) => (
-                            <DropdownMenuItem
-                                key={st.id}
-                                onSelect={() => setSelectedStore(st)}
-                                onClick={() => onStoreChange(st)}
-                                className="cursor-pointer"
-                            >
-                                {st.name}
-                                {st === selectedStore && <Check className="ml-auto" />}
-                            </DropdownMenuItem>
-                        ))}
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </SidebarMenuItem>
-        </SidebarMenu>
-    )
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu onOpenChange={setMenuOpen}>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+            >
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-accent">
+                <GalleryVerticalEnd className="size-4" />
+              </div>
+              <div className="flex flex-col gap-0.5 leading-none">
+                <span className="font-semibold">Loja</span>
+                <span>
+                  {selectedStore ? selectedStore.name : "Selecione uma loja"}
+                </span>
+              </div>
+              <ChevronDown
+                className={`ml-auto transition-transform duration-500 ${
+                  menuOpen ? "rotate-180" : ""
+                }`}
+              />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[var(--radix-dropdown-menu-trigger-width)] bg-[var(--muted)] text-[var(--foreground)]"
+            align="start"
+          >
+            {stores.map((st) => {
+              const isSelected = st === selectedStore;
+              return (
+                <DropdownMenuItem
+                  key={st.id}
+                  onSelect={() => setSelectedStore(st)}
+                  onClick={() => onStoreChange(st)}
+                  className={`cursor-pointer ${
+                    isSelected ? "bg-sidebar text-[var(--foreground)]" : "hover:bg-sidebar hover:text-foreground "
+                  }`}
+                >
+                  {st.name}
+                  {isSelected && <Check className="ml-auto" />}
+                </DropdownMenuItem>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
 }
